25ad7010a0cb177ff042090974cd4d890f008b14
Nicolas Burbinski <nburbinski@gmail.com>
2022-02-09 15:02:58 -0500

    search by food

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+const { count } = require("console");
 const express = require("express");
 const router = express.Router();
 const Food = require("../models/FoodSchema");
@@ -16,21 +17,26 @@ router.get("/", async (req, res) => {
 // @desc Search one food
 // @route GET /api/foods/:name
 router.get("/search", async (req, res) => {
-  const name = req.query.name;
-  try {
-    let food = await Food.findOne({ name: name });
+  const name = req.query.name.trim().toLowerCase();
 
-    if (food)
+  try {
+    const food = await Food.findOne({ name: name });
+    console.log(food);
+    if (food) {
+      await Food.findByIdAndUpdate(food._id, { count: food.count + 1 });
       res.render("food", {
+        food: food,
         name: name,
         edible: food.edible,
+        description: food._doc.description,
       });
-    else {
+    } else {
       res.render("notFound", {
         name: name,
       });
     }
   } catch (err) {
+    console.log(err);
     res.status(400).json({ msg: `${name} not currently listed ` });
   }
 });
